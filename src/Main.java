7e8755da353d96080fff8c895643f51c706749ee
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-09-17 10:14:08 -0500

    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        int counter=10;
 
-
-
+        for (int repeticiones = 0; repeticiones<=counter; repeticiones++  ) {
+            System.out.println(Fibrecurns(repeticiones)+ " ");
+        }
     }
-
-    public void Fibrecurns(int n) {
-
-    }
-    public void FibList(){
-
+    public static int Fibrecurns(int n){
+        if (n<=1){
+            return n;
+        }else {
+            return Fibrecurns(n-1)+Fibrecurns(n-2);
+        }
     }
 }
